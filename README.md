e5fe306940a6e6d4d022837b11121d50275b0b05
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2020-04-06 21:40:13 +0200

    code cleanup

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ Create a "datasets" folder in the directory and extract these datasets in it. ht
 
 Then open train.py and change the dataset variable. You can also play around with the number of trainnings and number of hidden nodes in each layer to find the best combination. 
 
-To run use: python train.py
+To create a nework run: ```python train.py <datasetName> <saveName> <iterations>```
+```datasetName``` = Name of the dataset i.e. mnist/letter/balanced/digits/byclass/bymerge
+```saveName``` = Nework will saved with this name in the networks folder
+```iteretion``` = number of iterations for the training
+
+To test a nework run: ```python test.py <datasetName> <saveName>```
+```datasetName``` = Name of the dataset i.e. mnist or letters
+```saveName``` = Name of the saved network in the networks folder without the extension
+
---
+++ load.py
@@ -0,0 +1,31 @@
+import numpy as np
+import gzip
+
+def load_data(dataset):
+    def load_images(filename):
+
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        # The inputs are vectors now, we reshape them to monochrome 2D images,
+        # following the shape convention: (examples, channels, rows, columns)
+        data = data.reshape(-1, 1, 28, 28)
+        # The inputs come as bytes, we convert them to float32 in range [0,1].
+        return data / np.float32(256)
+
+    def load_labels(filename):
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=8)
+        # The labels are vectors of integers now, that's exactly what we want.
+        return data
+
+    train_labels = load_labels(
+        './datasets/emnist-{}-train-labels-idx1-ubyte.gz'.format(dataset))  # Y
+    train_images = load_images(
+        './datasets/emnist-{}-train-images-idx3-ubyte.gz'.format(dataset))  # X
+    test_labels = load_labels(
+        './datasets/emnist-{}-test-labels-idx1-ubyte.gz'.format(dataset))   # Y
+    test_images = load_images(
+        './datasets/emnist-{}-test-images-idx3-ubyte.gz'.format(dataset))  # X
+
+
+    return train_images, train_labels, test_images, test_labels
